24b48242938a5b07d221641f5ba5ec53db973198
Waner Miranda <wanermiranda@gmail.com>
2015-05-10 23:27:32 -0300

    orb

--- tp2.py
+++ tp2.py
@@ -9,6 +9,13 @@ import threading
 import time
 import matplotlib.pyplot as plt
 
+MAX_HEIGHT = 2000.00
+
+SIFT_SIFT = 0
+FAST_BRIEF = 1
+ORB_ORB = 2
+
+CURRENT_METHOD = SIFT_SIFT
 
 IMG_COUNT = 6
 IMG_PREFIX = 'img'
@@ -16,22 +23,20 @@ IMG_EXT = 'jpg'
 IMG_GROUND_TRUTH = 'groundtruth.jpg'
 FLANN_INDEX_KDTREE = 1
 
-# Funcao de suporte para salvar a imagem
+
+# Support function to save images to files
 def save_image(img, name):
     plt.imshow(img)
     plt.savefig(name)
 
 
-# funcao de suporte para exibicao de imagem
+# Support function to show images
 def show_image(img):
-    plt.imshow(img, cmap=plt.cm.Greys_r)
+    plt.imshow(img)
     plt.show()
 
 
-# Funcao de suporte para exibicao da imagem
-# funcao que faz o calculo do histograma de cada canal de cor
-# utilizado somente a funcao do opencv por ser mais rapida,
-# mas o algoritmo segue abaixo como demonstracao
+# Support function to calc color histograms, if needed
 def image_hist(img):
     colors_hist = np.zeros((3, 256))
     for i in range(0, 3):
@@ -41,7 +46,7 @@ def image_hist(img):
     return colors_hist
 
 
-# funcao de suporte para o calculo do erro quadratico medio
+# Support function to calculate the mean squared error
 def get_mse(query, target):
     (m, n) = query.shape[:2]
     if (m == 3) and (n == 256):
@@ -53,8 +58,7 @@ def get_mse(query, target):
     return sums / (m * n)
 
 
-# funcao de suporte para calcular a relacao entre uma
-# imagem alvo, uma imagem de pesquisa para decidir o tamanho da janela de percorrer
+# Support function to define the correlation factor between to images
 def get_size_factor(query, target, factor):
     (target_h, target_w) = target.shape[:2]
     (query_h, query_w) = query.shape[:2]
@@ -65,8 +69,7 @@ def get_size_factor(query, target, factor):
     return factor
 
 
-# funcao de suporte para recuperar qual a relacao entre o tamanho da imagem alvo
-# e o tamanho desejado para redimensionamento
+# Support function to define if the image surpass a limited size
 def get_base_scale(img, maximum):
     (query_h, query_w) = img.shape[:2]
     if query_h > query_w:
@@ -76,13 +79,60 @@ def get_base_scale(img, maximum):
     return factor
 
 
-# filtro de passa baixa
+# Low pass filter
 def remove_noise(img):
     kernel = np.ones((3, 3), np.float32) / 8
     dst = cv2.filter2D(img, -1, kernel)
     return dst
 
 
+# To keep the code clear applied to design patterns, decorator and singleton
+# since the method is selected once in a run
+def singleton(cls):
+    instances = {}
+
+    def get_instance():
+        if cls not in instances:
+            instances[cls] = cls()
+        return instances[cls]
+
+    return get_instance
+
+
+# The class used to collect the method used in the run, with its detectors and matcher
+@singleton
+class Method:
+    def __init__(self):
+        self._method = -1
+        self._detector = None
+        self._matcher = None
+
+    def set(self, method=SIFT_SIFT):
+        self._method = method
+        print method
+        if method == SIFT_SIFT:
+            self._detector = cv2.xfeatures2d.SIFT_create()
+            index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+            search_params = dict(checks=64)
+            # Faster than brute force, considering that sift is a very expensive detector
+            self._matcher = cv2.BFMatcher()#cv2.FlannBasedMatcher(index_params, search_params)
+        else:
+            if method == ORB_ORB:
+                self._detector = cv2.ORB_create()
+                # HAMMING because orb create a set binary descriptors
+                self._matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    def get_detector(self):
+        return self._detector
+
+    def get_matcher(self):
+        return self._matcher
+
+    def get_method(self):
+        return self._method
+
+
+# The class responsible for the all the metadata and operations over the tiles to be compacted into a panorama
 class ImageBlock:
     def __init__(self, path=None, image=None):
         self._descriptors = None
@@ -98,10 +148,9 @@ class ImageBlock:
 
         h, w = self._image.shape[:2]
 
-        if h > 800:
-            scale = 800.00 / h
+        if h > MAX_HEIGHT:
+            scale = MAX_HEIGHT / h
             self._image = cv2.resize(self._image, None, fx=scale, fy=scale)
-
 
     def get_image(self):
         return self._image
@@ -114,17 +163,6 @@ class ImageBlock:
 
     def get_path(self):
         return self._path
-
-    @staticmethod
-    def get_detector():
-        return cv2.xfeatures2d.SIFT_create()
-
-    @staticmethod
-    def get_matcher():
-        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=4)
-        search_params = dict(checks=32)
-        matcher = cv2.FlannBasedMatcher(index_params, search_params)
-        return matcher
 
     def detect(self, left=None, right=None):
 
@@ -140,10 +178,10 @@ class ImageBlock:
             mask[:, :left] = 255
 
         if right is not None:
-            mask[img_h-right:, :] = 255
+            mask[img_h - right:, :] = 255
 
         # detecting key points and regions
-        detector = self.get_detector()
+        detector = Method().get_detector()
 
         print "detect " + self.get_path()
         kp = detector.detect(gray, mask)
@@ -157,15 +195,27 @@ class ImageBlock:
         return (homography[0, 1] * homography[1, 1] * homography[2, 2]) - (
             homography[2, 0] * homography[1, 1] * homography[0, 2])
 
-    def match(self, target, threshold=0.8):
-        print "Matching"
-        matcher = self.get_matcher()
-        knn_matches = matcher.knnMatch(self._descriptors, target.get_descriptors(), k=2)
-
+    @staticmethod
+    def get_good_matches(matches, threshold):
         good = []
-        for m, n in knn_matches:
-            if m.distance < threshold * n.distance:
-                good.append(m)
+        if Method().get_method() == SIFT_SIFT:
+            for m, n in matches:
+                if m.distance < threshold * n.distance:
+                    good.append(m)
+        else:
+            good = matches[:20]
+
+        return good
+
+    def match(self, target, threshold=0.7):
+        print "Matching"
+        matcher = Method().get_matcher()
+        if Method().get_method() == SIFT_SIFT:
+            matches = matcher.knnMatch(self._descriptors, target.get_descriptors(), k=2)
+        else:
+            matches = matcher.match(self._descriptors, target.get_descriptors())
+
+        good = self.get_good_matches(matches, threshold)
         return good
 
     def get_homography(self, target, good_matches):
@@ -183,9 +233,10 @@ class ImageBlock:
         return matrix_h
 
 
-
 class Mosaic:
-    def get_wrap_image(self, center_block, target):
+
+    @staticmethod
+    def get_wrap_image(center_block, target):
         print "Matching " + center_block.get_path() + " and " + target.get_path()
         matches = center_block.match(target)
         homography = center_block.get_homography(target, matches)
@@ -208,13 +259,14 @@ class Mosaic:
         img_h = int(math.ceil(max_y))
         target_img_wrp = cv2.warpPerspective(target.get_image(), mod_inv_h, (img_w, img_h))
 
-        (ret, data_map) = cv2.threshold(cv2.cvtColor(target_img_wrp, cv2.COLOR_BGR2GRAY),
-                                        0, 255, cv2.THRESH_BINARY)
         # img = cv2.half()
         # cv::Mat half(result,cv::Rect(0,0,image2.cols,image2.rows));
         # show_image(target_img_wrp)
 
         center_img_wrp = cv2.warpPerspective(center_block.get_image(), move_h, (img_w, img_h))
+
+        (ret, data_map) = cv2.threshold(cv2.cvtColor(center_img_wrp, cv2.COLOR_BGR2GRAY),
+                                        0, 255, cv2.THRESH_BINARY)
 
         enlarged_base_img = np.zeros((img_h, img_w, 3), np.uint8)
         # Now add the warped image
@@ -222,7 +274,9 @@ class Mosaic:
         save_image(center_img_wrp, 'wrapped_' + center_block.get_path())
 
         enlarged_base_img = cv2.add(enlarged_base_img, target_img_wrp,
+                                    mask=np.bitwise_not(data_map),
                                     dtype=cv2.CV_8U)
+
         save_image(enlarged_base_img, 'wrapped_1_' + center_block.get_path())
 
         final_img = cv2.add(enlarged_base_img, center_img_wrp,
@@ -240,9 +294,7 @@ class Mosaic:
         return result_block
 
     def __init__(self):
-
-        center = (IMG_COUNT + 1) / 2
-
+        Method().set(CURRENT_METHOD)
         block_list = []
         for img_idx in range(1, IMG_COUNT + 1):
             img_path = IMG_PREFIX + str(img_idx) + "." + IMG_EXT
@@ -257,31 +309,20 @@ class Mosaic:
         block34.detect(left=3000)
         block234 = self.combine(block34, block_list[1], 'img234.png')
         del block34
-        block234.detect(left=3000)
-        block1234 = self.combine(block234, block_list[0], 'img1234.png')
+
+        block234.detect(right=3000)
+        block2345 = self.combine(block234, block_list[4], 'img2345.png')
         del block234
-        block1234.detect(right=3000)
-        block12345 = self.combine(block1234, block_list[4], 'img12345.png')
-        del block1234
+
+        block2345.detect(left=3000)
+        block12345 = self.combine(block2345, block_list[0], 'img12345.png')
+        del block2345
+
         block12345.detect(right=3000)
         block123456 = self.combine(block12345, block_list[5], 'img123456.png')
         del block12345
-        # block123456.detect()
         show_image(block123456.get_image())
         del block123456
-        # left = block_list[2]
-        # center_block = block_list[3]
-        # right = block_list[4]
-        #
-        # center_img_1 = self.left_right(center_block, left, right)
-        # show_image(center_img_1)
-        # left = block_list[1]
-        # right = block_list[5]
-        # center_block_1 = ImageBlock('center_image_1', center_img_1)
-        #
-        # center_img_2 = self.left_right(center_block_1, left, right)
-
-        #show_image(center_img_2)
 
 
 def findDimensions(image, homography):
@@ -335,28 +376,28 @@ __author__ = 'gorigan'
 
 
 
-#junk yard
+# junk yard
 
-        # def print_lines(self, target, homography, mask, good_matches):
-        #
-        # matches_mask = mask.ravel().tolist()
-        #
-        #     h, w = self._image.shape[:2]
-        #     pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
-        #     dst = cv2.perspectiveTransform(pts, homography)
-        #
-        #     target_img = cv2.polylines(target.get_image(), [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
-        #
-        #     # draw_params = dict(matchColor=(0, 255, 0),
-        #     #                    singlePointColor = None,
-        #     #                    matchesMask = matches_mask,
-        #     #                    flags = 2)
-        #
-        #     result = cv2.drawMatches(  #self._image, self._keypoints,
-        #                                self._image, None,
-        #                                #target_img, target.get_keypoints(),
-        #                                target_img, None,
-        #                                None, None, None)
-        #     #good_matches, None, **draw_params)
-        #
-        #     show_image(result)
+# def print_lines(self, target, homography, mask, good_matches):
+#
+# matches_mask = mask.ravel().tolist()
+#
+#     h, w = self._image.shape[:2]
+#     pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+#     dst = cv2.perspectiveTransform(pts, homography)
+#
+#     target_img = cv2.polylines(target.get_image(), [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+#
+#     # draw_params = dict(matchColor=(0, 255, 0),
+#     #                    singlePointColor = None,
+#     #                    matchesMask = matches_mask,
+#     #                    flags = 2)
+#
+#     result = cv2.drawMatches(  #self._image, self._keypoints,
+#                                self._image, None,
+#                                #target_img, target.get_keypoints(),
+#                                target_img, None,
+#                                None, None, None)
+#     #good_matches, None, **draw_params)
+#
+#     show_image(result)
